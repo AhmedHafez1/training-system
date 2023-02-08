7a9c095dd1512bbb00eb1d47117b0c73d4d6975a
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-02-08 08:03:55 +0200

    Added the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { courses, COURSES } from './shared/courses';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, CourseListComponent, HeaderComponent],
@@ -17,7 +19,9 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     MatCardModule,
     MatToolbarModule,
+    MatButtonModule,
     BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [{ provide: COURSES, useValue: courses }],
   bootstrap: [AppComponent],
